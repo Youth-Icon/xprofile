ac6a29063f27232971087b08d3c3420143c510e0
Mohammad Faizan <faizancurious@gmail.com>
2023-09-11 08:45:31 +0530

    Sheet minor fix

--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -27,7 +27,7 @@ const SheetOverlay = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <SheetPrimitive.Overlay
     className={cn(
-      "fixed inset-0 z-50 bg-white/80 backdrop-blur-sm data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 dark:bg-slate-950/80",
+      "fixed inset-0 z-50 bg-white/80 backdrop-blur-sm data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 dark:bg-[#121212]/80",
       className
     )}
     {...props}
