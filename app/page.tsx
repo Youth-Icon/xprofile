278bd2d58ee6e244529aa1b7ef0114d283be7899
Mohammad Faizan <faizancurious@gmail.com>
2023-09-11 07:38:25 +0530

    Started working on Nav Bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,20 @@
 import Image from 'next/image'
 import { ModeToggle } from '@/components/toggle-btn'
 import Link from 'next/link'
+import { Nav } from '@/components/Nav'
 export default function Home() {
   return (
     <>
-      <div className='flex flex-col items-center justify-center min-h-screen py-2 '>
-        <p className="scroll-m-20 hover:text-green-500 text-3xl font-semibold tracking-tight">
-          <Link href='https://x.hellofaizan.me'>
-            Under construction 🚧
-          </Link>
-        </p>
-      </div>
+      <main className='flex flex-col'>
+        <Nav />
+        <div className='flex flex-col items-center justify-center min-h-screen py-2 '>
+          <p className="scroll-m-20 hover:text-green-500 text-3xl font-semibold tracking-tight">
+            <Link href='https://x.hellofaizan.me'>
+              Under construction 🚧
+            </Link>
+          </p>
+        </div>
+      </main>
 
       <div className='absolute top-10 right-10'>
         <ModeToggle />
---
+++ components/Nav.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Nav = () => {
+  return (
+    <div>Nav</div>
+  )
+}
+
