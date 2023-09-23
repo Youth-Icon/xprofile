69f3a1c35c244b26a64a37f4e3c465f07aa48460
qryskalyst20 <quddusnuriman@gmail.com>
2023-09-24 00:01:17 +0800

    :wrench: Import hostname from github images

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        pathname: "**",
+        port: "",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
