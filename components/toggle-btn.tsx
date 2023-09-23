c03a9a2de99a6e446b00618e5001df61949bffbd
qryskalyst20 <quddusnuriman@gmail.com>
2023-09-23 15:23:15 +0800

    :lipstick: Simplify theme toggle button

--- components/toggle-btn.tsx
+++ components/toggle-btn.tsx
@@ -3,14 +3,7 @@
 import * as React from "react";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
-
 import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 
 export function ModeToggle() {
   const { theme, setTheme } = useTheme();
