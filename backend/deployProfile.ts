2e1f9985c88794ae39544db90880d4c6babb061d
qryskalyst20 <quddusnuriman@gmail.com>
2023-09-24 22:34:34 +0800

    :white_check_mark: Insert data from form to supabase table

---
+++ backend/deployProfile.ts
@@ -0,0 +1,29 @@
+"use server";
+
+import supabase from "./supabase";
+
+export async function deployProfile(userData: FormData) {
+  //   console.log(userData.getAll("tags"));
+  const { data, error } = await supabase.from("profiles").insert([
+    {
+      xusername: userData.get("twitter"),
+      name: userData.get("username"),
+      location: "dummy loc",
+      github: userData.get("github"),
+      banner_color: userData.get("color"),
+      about: userData.get("description"),
+      tags: userData.getAll("tags"),
+      socials: ["dummy socials"],
+      repos: ["dummy repos"],
+      links: ["dummy links"],
+    },
+  ]);
+
+  if (error) {
+    console.log(error);
+  }
+
+  if (data) {
+    console.log(data);
+  }
+}
