c812e2592ae79b12ab9feeafc2af5e32250609e0
qryskalyst20 <quddusnuriman@gmail.com>
2023-09-27 00:50:52 +0800

    :alien: Migrating to firebase

--- backend/deployProfile.ts
+++ backend/deployProfile.ts
@@ -1,6 +1,7 @@
 "use server";
 
-import supabase from "./supabase";
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "./firebase";
 
 export async function deployProfile(userData: FormData) {
   const githubstars = await fetch(
@@ -19,28 +20,43 @@ export async function deployProfile(userData: FormData) {
       return data.followers;
     });
 
-  const { data, error } = await supabase.from("profiles").insert([
-    {
-      xusername: userData.get("twitter"),
-      name: userData.get("username"),
-      location: userData.get("location"),
-      github: userData.get("github"),
-      banner_color: userData.get("color"),
-      about: userData.get("description"),
-      tags: userData.getAll("tags"),
-      socials: userData.getAll("socials"),
-      repos: ["dummy repos"],
-      links: ["dummy links"],
-      github_stars: githubstars,
-      github_followers: githubfollowers,
-    },
-  ]);
+  await addDoc(collection(db, "profiles"), {
+    xusername: userData.get("twitter"),
+    name: userData.get("username"),
+    location: userData.get("location"),
+    github: userData.get("github"),
+    banner_color: userData.get("color"),
+    about: userData.get("description"),
+    tags: userData.getAll("tags"),
+    socials: userData.getAll("socials"),
+    repos: ["dummy repos"],
+    links: ["dummy links"],
+    github_stars: githubstars,
+    github_followers: githubfollowers,
+  });
 
-  if (error) {
-    console.log(error);
-  }
+  // const { data, error } = await supabase.from("profiles").insert([
+  //   {
+  //     xusername: userData.get("twitter"),
+  //     name: userData.get("username"),
+  //     location: userData.get("location"),
+  //     github: userData.get("github"),
+  //     banner_color: userData.get("color"),
+  //     about: userData.get("description"),
+  //     tags: userData.getAll("tags"),
+  //     socials: userData.getAll("socials"),
+  //     repos: ["dummy repos"],
+  //     links: ["dummy links"],
+  //     github_stars: githubstars,
+  //     github_followers: githubfollowers,
+  //   },
+  // ]);
 
-  if (data) {
-    console.log(data);
-  }
+  // if (error) {
+  //   console.log(error);
+  // }
+
+  // if (data) {
+  //   console.log(data);
+  // }
 }
