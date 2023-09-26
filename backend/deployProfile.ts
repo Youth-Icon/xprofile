5df10a7e7acc37b9a9a716898b97789822fddabf
qryskalyst20 <quddusnuriman@gmail.com>
2023-09-26 20:59:10 +0800

    :alien: Pass fetched followers count and most stars

--- backend/deployProfile.ts
+++ backend/deployProfile.ts
@@ -3,6 +3,22 @@
 import supabase from "./supabase";
 
 export async function deployProfile(userData: FormData) {
+  const githubstars = await fetch(
+    `https://api.github-star-counter.workers.dev/user/${userData.get("github")}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      return data.stars;
+    });
+
+  const githubfollowers = await fetch(
+    `https://api.github.com/users/${userData.get("github")}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      return data.followers;
+    });
+
   const { data, error } = await supabase.from("profiles").insert([
     {
       xusername: userData.get("twitter"),
@@ -15,6 +31,8 @@ export async function deployProfile(userData: FormData) {
       socials: userData.getAll("socials"),
       repos: ["dummy repos"],
       links: ["dummy links"],
+      github_stars: githubstars,
+      github_followers: githubfollowers,
     },
   ]);
 
