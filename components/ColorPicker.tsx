616f9a3beb9a75c91c1dd9f92d597066d0577ac7
qryskalyst20 <quddusnuriman@gmail.com>
2023-09-28 11:15:24 +0800

    :fire: Removes unnecessary lines

--- components/ColorPicker.tsx
+++ components/ColorPicker.tsx
@@ -7,10 +7,8 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { cn } from "@/lib/utils";
 import { Paintbrush } from "lucide-react";
-import { useMemo } from "react";
 
 interface ColorPickerProps {
   data: {
@@ -58,38 +56,6 @@ export function GradientPicker({
     "#cd93ff",
     "#09203f",
   ];
-
-  const gradients = [
-    "linear-gradient(to top left,#accbee,#e7f0fd)",
-    "linear-gradient(to top left,#d5d4d0,#d5d4d0,#eeeeec)",
-    "linear-gradient(to top left,#000000,#434343)",
-    "linear-gradient(to top left,#09203f,#537895)",
-    "linear-gradient(to top left,#AC32E4,#7918F2,#4801FF)",
-    "linear-gradient(to top left,#f953c6,#b91d73)",
-    "linear-gradient(to top left,#ee0979,#ff6a00)",
-    "linear-gradient(to top left,#F00000,#DC281E)",
-    "linear-gradient(to top left,#00c6ff,#0072ff)",
-    "linear-gradient(to top left,#4facfe,#00f2fe)",
-    "linear-gradient(to top left,#0ba360,#3cba92)",
-    "linear-gradient(to top left,#FDFC47,#24FE41)",
-    "linear-gradient(to top left,#8a2be2,#0000cd,#228b22,#ccff00)",
-    "linear-gradient(to top left,#40E0D0,#FF8C00,#FF0080)",
-    "linear-gradient(to top left,#fcc5e4,#fda34b,#ff7882,#c8699e,#7046aa,#0c1db8,#020f75)",
-    "linear-gradient(to top left,#ff75c3,#ffa647,#ffe83f,#9fff5b,#70e2ff,#cd93ff)",
-  ];
-
-  const images = [
-    "url(https://images.unsplash.com/photo-1691200099282-16fd34790ade?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2532&q=90)",
-    "url(https://images.unsplash.com/photo-1691226099773-b13a89a1d167?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2532&q=90",
-    "url(https://images.unsplash.com/photo-1688822863426-8c5f9b257090?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2532&q=90)",
-    "url(https://images.unsplash.com/photo-1691225850735-6e4e51834cad?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2532&q=90)",
-  ];
-
-  const defaultTab = useMemo(() => {
-    if (background.includes("url")) return "image";
-    if (background.includes("gradient")) return "gradient";
-    return "solid";
-  }, [background]);
 
   return (
     <Popover>
