39ba54422a8de990a19e69299003ef2acfeda577
qryskalyst20 <quddusnuriman@gmail.com>
2023-09-29 01:48:53 +0800

    :sparkles: Fetch user data from database based on twitter username

--- app/user/[user]/UserPage.tsx
+++ app/user/[user]/UserPage.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { useState, useEffect } from "react";
-import { db } from "@/backend/firebase";
+import { useState, useEffect, useTransition } from "react";
+import { GetUserData } from "@/backend/GetUserData";
 
 type Params = {
   params: {
@@ -10,9 +10,25 @@ type Params = {
 };
 
 const UserPage = ({ params }: Params) => {
+  const [userData, setUserData] = useState<any>(null);
+  const [isPending, startTransition] = useTransition();
+
+  useEffect(() => {
+    try {
+      startTransition(async () => {
+        const data = await GetUserData({ user: params.user });
+        setUserData(data);
+      });
+    } catch (err) {
+      console.log(err);
+    }
+
+    // setUserData(data);
+  }, [params.user]);
+
   return (
     <div className="flex md:flex-row flex-col gap-5 md:gap-0 w-full min-h-screen pt-20">
-      <p>User: {params.user}</p>
+      {isPending ? <p>Loading...</p> : <pre>{JSON.stringify(userData)}</pre>}
     </div>
   );
 };
