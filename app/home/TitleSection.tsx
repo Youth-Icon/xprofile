abf66258e2b83a105bd027b9cc4d8c31d8932cb0
yuvarajsingh-0 <yuvarajsingh170@gmail.com>
2023-10-07 16:36:19 +0530

    finishing touches

--- app/home/TitleSection.tsx
+++ app/home/TitleSection.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 
-export default function TitleSection(){
+export default function TitleSection() {
     return (
         <div className="md:my-44">
             <div className="text-center mt-32 px-1">
@@ -9,7 +9,12 @@ export default function TitleSection(){
                 <p className=" text-xl md:text-lg leading-8 mb-20 md:px-[22%] text-[rgba(209,213,219,1)] mx-auto">Everything you&apos;re, in one simple link in bio you&apos;ll need to showcase your all socials & portfolio in one place</p>
             </div>
             <div className="flex gap-3 md:px-20 px-0  justify-center">
-                <input className="bg-[rgba(255,255,255,0.94)] md:text-base md:px-6 md:py-3 rounded-lg dark:text-slate-900" />
+                <div className="flex">
+                    <div className="bg-[rgba(255,255,255,0.94)] md:text-base font-medium  md:pl-6 md:py-3 rounded-s-lg dark:text-slate-900">
+                        xlink.io/
+                    </div>
+                    <input placeholder="yourusername" className="bg-[rgba(255,255,255,0.94)] outline-none font-medium md:pr-6 md:py-3 rounded-e-lg dark:text-slate-900" />
+                </div>
 
                 <Button variant={'outline'} className={cn("md:px-6 md:py-3  md:text-base w-max border-slate-600 rounded-full my-auto h-full font-thin")}>
                     Claim Your XLink
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -13,7 +13,7 @@ const buttonVariants = cva(
         destructive:
           "bg-red-500 text-slate-50 hover:bg-red-500/90 dark:bg-red-900 dark:text-slate-50 dark:hover:bg-red-900/90",
         outline:
-          "border border-slate-200 bg-[transaparent] hover:bg-slate-100 hover:text-slate-900 dark:border-slate-600 dark:hover:bg-zinc-800 dark:hover:text-slate-50",
+          "border border-slate-200 bg-[transaparent] hover:bg-slate-100 hover:text-slate-900 dark:border-slate-600 dark:hover:bg-slate-800 dark:hover:text-slate-50",
         secondary:
           "bg-slate-100 text-slate-900 hover:bg-slate-100/80 dark:bg-slate-800 dark:text-slate-50 dark:hover:bg-slate-800/80",
         ghost: "hover:bg-slate-100 hover:text-slate-900 dark:hover:bg-slate-800 dark:hover:text-slate-50",
