222228f4371b33af3f1210a820f8045b07ebe5f6
Aman Raj Rana <mail2amanrana@gmail.com>
2023-10-07 12:33:08 +0530

    Implement: Fetch userdata from the backend

--- app/(user-public-profile)/[username]/page.tsx
+++ app/(user-public-profile)/[username]/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { MdVerified } from "react-icons/md";
 import { HiUser } from "react-icons/hi";
 import { BsTwitter } from "react-icons/bs";
@@ -8,25 +10,74 @@ import {
 } from "react-icons/ai";
 import LinkPlate from "./components/LinkPlate";
 import TagChips from "./components/TagChips";
+import { startTransition, useEffect, useState } from "react";
+import { GetUserData } from "@/backend/GetUserData";
+import Image from "next/image";
 
-const page = () => {
+type UserPublicProfileProps = {
+  params: { username: string };
+};
+
+const UserPublicProfile = ({ params }: UserPublicProfileProps) => {
+  type UserObjectType = {
+    about: string;
+    image: string;
+    location: string;
+    slug: string;
+    github_username: string;
+    name: string;
+    twitter: string;
+    github_stars: number;
+    github_followers: number;
+    banner_color: string;
+    links: any[]; // Adjust as needed
+    socials: {
+      type: string;
+      link: string;
+    }[];
+  };
+
+  const [userData, setUserData] = useState<UserObjectType>();
+
+  useEffect(() => {
+    try {
+      startTransition(() => {
+        GetUserData({ user: params.username })
+          .then((data) => {
+            const userData = data as UserObjectType;
+            setUserData(userData);
+          })
+          .catch((error: string) => {
+            console.log(error);
+          });
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  console.log("userData", userData);
+
   return (
     <section className="dark:bg-gradient-to-r dark:from-black dark:via-slate-900 dark:to-black  ">
       <div className="w-full h-32 md:h-48 bg-purple-700"></div>
       <div className="border-white flex flex-col items-center px-3">
-        <HiUser
-          size={150}
-          className="text-slate-700 rounded-full bg-white border-8 border-white relative -translate-y-1/2"
+        <Image
+          src={userData?.image || "/images/placeholder.png"}
+          alt={userData?.name || "User Image"}
+          width={150}
+          height={150}
+          className="rounded-full relative border -translate-y-1/2"
         />
 
         <div className="relative -translate-y-14 flex items-center flex-col w-max max-w-full space-y-6">
           <div className="flex flex-col items-center mx-auto">
             <h1 className="text-4xl font-semibold">
-              Amena Rahul
+              {userData?.name}
               <MdVerified className="text-blue-600 inline ml-1 text-2xl" />
             </h1>
-            <p className="text-gray-500 text-xl -mt-0.5">@ayesha.tech 👋</p>
-            <p className="text-lg mt-2">UI/UX Designer | No-code Learner</p>
+            <p className="text-gray-500 text-xl -mt-0.5">{userData?.slug} 👋</p>
+            <p className="text-lg mt-2">{userData?.about}</p>
           </div>
 
           {/* tags */}
@@ -62,4 +113,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default UserPublicProfile;
