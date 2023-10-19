dc78c21a5cad2a14ccacae646ea61325d2702ed9
Federico <federicoc03@gmail.com>
2023-10-18 19:11:54 -0500

    fix: :lipstick: Improved_ResponsiveHome

--- app/(home)/TitleSection.tsx
+++ app/(home)/TitleSection.tsx
@@ -3,13 +3,14 @@ import { cn } from "@/lib/utils";
 
 export default function TitleSection() {
     return (
-        <div className="md:my-44 mx-1">
-            <div className="text-center mt-32 px-1">
+        <div className="flex flex-col items-center w-full mx-1 md:my-44">
+            <div className="w-full px-1 mt-32 text-center">
                 <h1 className="md:text-7xl py-10 text-4xl my-2 md:px-[17%] font-semibold h-max mt-14 mx-auto bg-gradient-to-r dark:from-slate-500 from-black dark:via-slate-200 dark:to-slate-500 to-black/50 bg-clip-text text-[rgba(0,0,0,0)]">The One Link for Everything</h1>
                 <p className=" text-sm md:text-lg leading-6 mb-20 md:px-[22%] dark:text-[rgba(209,213,219,1)] text-gray-900 mx-auto">Everything you&apos;re, in one simple link in bio you&apos;ll need to showcase your all socials & portfolio in one place</p>
             </div>
-            <div className="flex flex-col md:flex-row gap-3 px-4 justify-center items-center">
-                <div className="flex w-full md:w-max">
+
+            <div className="flex flex-col items-center justify-center w-4/5 gap-3 px-4 md:flex-row">
+                <div className="flex justify-center w-full md:w-max">
                     <div className="dark:bg-[rgba(255,255,255,0.94)] bg-black/5 md:text-base font-medium pl-4 border-none outline-none md:pr-0 md:pl-6 py-3 md:py-3 rounded-s-lg dark:text-slate-900">
                         xprofile.me/
                     </div>
