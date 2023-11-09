ddf6f5f4531486079d63e385c238e68993aa60e7
Mohammad Faizan <faizancurious@gmail.com>
2023-11-09 21:36:09 +0530

    test with secret in auth

--- backend/auth.ts
+++ backend/auth.ts
@@ -63,20 +63,14 @@ export const authOptions: NextAuthOptions = {
       clientId: env.GOOGLE_ID,
       clientSecret: env.GOOGLE_SECRET,
     }),
-    
     /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
+     * ...add more providers here. */
   ],
-  // pages: {
-  //   signIn: "/auth/signin",
-  // },
+  secret: env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: "/auth/login",
+    signOut: "/auth/login",
+  },
 };
 
 /**
