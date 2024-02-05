f8078dc36975716a3097dd5989b144137816f333
Mohammad Faizan <faizancurious@gmail.com>
2024-02-05 14:11:42 +0530

    revert last commit

--- components/use-auth-form.tsx
+++ components/use-auth-form.tsx
@@ -19,7 +19,7 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
   const handleLogin = async () => {
     setIsLoading(true)
     try {
-      await signIn("github")
+      await signIn("github", { callbackUrl: "/" })
 
     } catch (error) {
       toast({
@@ -35,7 +35,7 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
   const handleGoogleLogin = async () => {
     setIsLoading(true)
     try {
-      await signIn("google")
+      await signIn("google", { callbackUrl: "/" })
 
     } catch (error) {
       toast({
