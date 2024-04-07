af3c0423c3733564b7c699d9d6e3691d6e726da7
Bashamega <adambashaahmednaji@gmail.com>
2024-04-07 07:26:56 +0200

    No Sync Scripts fix

--- app/(extras)/help/components/chatbot.tsx
+++ app/(extras)/help/components/chatbot.tsx
@@ -1,11 +1,11 @@
 
 import React from "react";
 import '@/app/fab.css'
-
+import Script from 'next/script'
 export function ChatBot(){
     return(
         <div>
-            <script src="https://embed.pickaxeproject.com/axe/scripts/fab/bundle.js"></script>
+            <Script src="https://embed.pickaxeproject.com/axe/scripts/fab/bundle.js"/>
             <div id="rendering-information" data-height="200px" data-width="30%" data-formid="CleanBot_Customer_Support_7M269" data-frame-source="https://embed.pickaxeproject.com/axe?id=CleanBot_Customer_Support_7M269&mode=embed_gold&host=beta&theme=light&opacity=100&font_header=Real+Head+Pro&size_header=30&font_body=Real+Head+Pro&size_body=16&font_labels=Real+Head+Pro&size_labels=14&font_button=Real+Head+Pro&size_button=16&c_fb=FFFFFF&c_ff=FFFFFF&c_fbd=888888&c_bb=228DD7&c_bt=FFFFFF&c_t=000000&s_ffo=100&s_bbo=100&s_f=minimalist&s_b=filled&s_t=2&s_to=1&s_r=2" data-title="CleanBot Customer Support"></div> 
         </div>
         
